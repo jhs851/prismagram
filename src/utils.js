a76218b2b29148c2ce6b1b82a814292af05a31c1
jhs851 <jhs851@naver.com>
2019-09-10 15:29:45 +0900

    #3.5 Passport JWT part Two

--- src/utils.js
+++ src/utils.js
@@ -4,7 +4,7 @@ dotenv.config({ path: path.resolve(__dirname, '.env') });
 
 import { adjectives, nouns } from './words';
 import nodemailer from 'nodemailer';
-import sgTransport from 'nodemailer-sendgrid-transport';
+import jwt from 'jsonwebtoken';
 
 export const generateSecret = () => {
     const randomNumber = Math.floor(Math.random() * adjectives.length);
@@ -41,3 +41,5 @@ export const sendSecretMail = (address, secret) => {
 
     return sendMail(email);
 };
+
+export const generateToken = (id) => jwt.sign({ id }, process.env.JWT_SECRET);
