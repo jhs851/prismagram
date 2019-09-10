93c7ef4b8b0a57ba9797bb620e901cfc8edd984d
jhs851 <jhs851@naver.com>
2019-09-10 14:51:21 +0900

    #3.3 sendMail Function with Nodemailer

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,43 @@
+import dotenv from 'dotenv';
+import path from 'path';
+dotenv.config({ path: path.resolve(__dirname, '.env') });
+
 import { adjectives, nouns } from './words';
+import nodemailer from 'nodemailer';
+import sgTransport from 'nodemailer-sendgrid-transport';
 
 export const generateSecret = () => {
     const randomNumber = Math.floor(Math.random() * adjectives.length);
 
     return `${adjectives[randomNumber]} ${nouns[randomNumber]}`;
+};
+
+const sendMail = (email) => {
+    const options = {
+        host: 'smtp.mailtrap.io',
+        port: 2525,
+        auth: {
+            user: process.env.MAIL_USERNAME,
+            pass: process.env.MAIL_PASSWORD
+        }
+    };
+
+    const client = nodemailer.createTransport(options);
+
+    try {
+        return client.sendMail(email);
+    } catch (e) {
+        console.log(e);
+    }
+};
+
+export const sendSecretMail = (address, secret) => {
+    const email = {
+        from: 'jhs851@prismgram.com',
+        to: address,
+        subject: '🔒 Login Secret for Prismgram 🔒',
+        html: `Hello! Your login secret it ${secret}.<br>Copy paste on the app/website to login.`
+    };
+
+    return sendMail(email);
 };
